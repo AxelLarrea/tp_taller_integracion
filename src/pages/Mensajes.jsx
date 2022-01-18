ed0a7fa2e73d8a2f1e27cc54d2ffda41e93cce6d
Axel Larrea <63266736+AxelLarrea@users.noreply.github.com>
2022-01-17 22:33:18 -0300

    Home creado

---
+++ src/pages/Mensajes.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const Mensajes = () => {
+    return (
+        <>
+            <Header/>
+        </>
+    );
+}
+ 
+export default Mensajes;
---
+++ src/pages/ObraSocial.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const ObraSocial = () => {
+    return (
+        <>
+            <Header/>
+        </>
+    );
+}
+ 
+export default ObraSocial;
